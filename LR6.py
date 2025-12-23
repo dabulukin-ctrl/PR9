6743f4769c6448f06c7a67960cb6bec8d9920aaa
Данил <dabulukin@gmail.com>
2025-12-24 02:03:30 +0300

    add autotests

---
+++ LR6.py
@@ -0,0 +1,18 @@
+class Calculator:
+
+    def add(self, a, b):
+        return a + b
+
+    def divide(self, a, b):
+        if b == 0:
+            raise ZeroDivisionError("деление на 0 не допустимо")
+        else:
+            return a / b
+
+    def is_prime_number(self, n):
+        if n <= 1:
+            return False
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
---
+++ LR6test.py
@@ -0,0 +1,29 @@
+import unittest
+from LR6 import Calculator
+
+class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = Calculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(1, 2), 3)
+        self.assertEqual(self.calc.add(-4, 5), 1)
+        self.assertEqual(self.calc.add(0, 0), 0)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(-9, 3), -3)
+        self.assertEqual(self.calc.divide(7, 2), 3.5)
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            self.calc.divide(0, 0)
+
+    def test_is_prime_number(self):
+        self.assertEqual(self.calc.is_prime_number(1),False)
+        self.assertEqual(self.calc.is_prime_number(15), False)
+        self.assertEqual(self.calc.is_prime_number(7), True)
+
+
+if __name__ == '__main__':
+    unittest.main()
